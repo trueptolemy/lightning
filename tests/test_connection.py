64e24030b87c508a21ca3305ff1d1d31abc89ca1
trueptolemy <823220586@qq.com>
2019-05-17 00:12:58 +0800

    Update test_connection.py

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1344,9 +1344,9 @@ def test_reenable_channel_with_sigs(node_factory, bitcoind):
     wait_for(lambda: [c['active'] for c in l2.rpc.listchannels()['channels']] == [False, False])
     l1.start()
     sync_blockheight(bitcoind, [l1, l2])
-#    l1.daemon.wait_for_log('WIRE_CHANNEL_GOT_ANNOUNCEMENT')
-#    l2.daemon.wait_for_log('permfail')
-#    assert not l1.daemon.is_in_log('=WIRE_ANNOUNCEMENT_SIGNATURES')
+    l1.daemon.wait_for_log('WIRE_CHANNEL_GOT_ANNOUNCEMENT')
+    l2.daemon.wait_for_log('permfail')
+    assert not l1.daemon.is_in_log('=WIRE_ANNOUNCEMENT_SIGNATURES')
 
     billboard = only_one(l1.rpc.listpeers(l2.info['id'])['peers'][0]['channels'])['status']
     assert billboard == ['CHANNELD_NORMAL:Funding transaction locked. Channel announced.']
