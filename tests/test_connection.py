fbd96a5f22a0ac8f4bbf9998801331b2858bee18
trueptolemy <823220586@qq.com>
2019-05-16 22:00:36 +0800

    fix

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1344,11 +1344,9 @@ def test_reenable_channel_with_sigs(node_factory, bitcoind):
     wait_for(lambda: [c['active'] for c in l2.rpc.listchannels()['channels']] == [False, False])
     l1.start()
 
-    time.sleep(10)
-
-    l2.daemon.wait_for_log('peer_in WIRE_CHANNEL_REESTABLISH')
     l2.daemon.wait_for_log('permfail')
     l1.daemon.wait_for_logs(['WIRE_CHANNEL_GOT_ANNOUNCEMENT'] * 2)
+    l2.daemon.wait_for_log('permfail')
     assert not l1.daemon.is_in_log('=WIRE_ANNOUNCEMENT_SIGNATURES')
 
     billboard = only_one(l1.rpc.listpeers(l2.info['id'])['peers'][0]['channels'])['status']
