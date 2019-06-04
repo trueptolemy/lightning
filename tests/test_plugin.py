c0a6ee03c0a6404d7c40c0882398b98fd1535715
trueptolemy <823220586@qq.com>
2019-06-05 01:54:27 +0800

    Update test_plugin.py

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -323,7 +323,7 @@ def test_unusual_event_notification(node_factory):
 
     l1.rpc.pretendunusual("Test unusual event notification")
 
-#    assert not l1.daemon.is_in_log('plugin-unusual_event.py Test unusual event notification')
+    l1.daemon.wait_for_log('plugin-unusual_event.py Test unusual event notification')
 
     l1.daemon.logsearch_start = 0
     l1.daemon.wait_for_log('plugin-unusual_event.py Received unusual log:')
