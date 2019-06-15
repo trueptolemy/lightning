6b212ab518bf72a68589085365d6791393cc6473
trueptolemy <823220586@qq.com>
2019-06-16 02:23:03 +0800

    Plugin: a simple plugin to test 'forward_event'

---
+++ tests/plugins/forward_payment_status.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""This plugin is used to check that forward_event calls are working correctly.
+"""
+from lightning import Plugin
+
+plugin = Plugin()
+
+
+def check(record, dbforward):
+    if record['status'] == 'offered':
+        if dbforward['status'] == 'local_failed':
+            record['failcode'] = dbforward['failcode']
+            record['failreason'] = dbforward['failreason']
+        elif dbforward['status'] != 'offered':
+            record['resolved_time'] = dbforward['resolved_time']
+    if record == dbforward:
+        return True
+    else:
+        return False
+
+
+@plugin.init()
+def init(configuration, options, plugin):
+    plugin.forward_list = []
+
+
+@plugin.subscribe("forward_event")
+def notify_warning(plugin, forward):
+    plugin.forward_list.append(forward)
+
+
+@plugin.method('recordcheck')
+def record_lookup(payment_hash, status, dbforward, plugin):
+    for forward in plugin.forward_list:
+        if forward['payment_hash'] == payment_hash and forward['status'] == status:
+            check_result = check(forward, dbforward)
+            return check_result
+    return False
+
+
+plugin.run()
