3ad218b5728ca49ff7321465f207b6f37b4183ef
trueptolemy <trueptolemy@foxmail.com>
2019-08-13 23:18:16 +0800

    Update walletrpc.c

--- wallet/walletrpc.c
+++ wallet/walletrpc.c
@@ -212,7 +212,7 @@ static struct command_result *json_prepare_tx(struct command *cmd,
 	} else
 		changekey = NULL;
 
-	log_debug(cmd->ld->log, "script: %s", outputs[0]->script);
+	log_debug(cmd->ld->log, "script: %.*s", tal_bytelen(outputs[0]->script));
 	log_debug(cmd->ld->log, "amount: %s", type_to_string(tmpctx, struct amount_sat, &(outputs[0]->amount)));
 	(*utx)->tx = withdraw_tx(*utx, get_chainparams(cmd->ld),
 				 (*utx)->wtx->utxos, outputs,
