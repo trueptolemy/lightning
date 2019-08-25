181c093351b97c1fd780c556f4ec007cf71e0e95
trueptolemy <trueptolemy@foxmail.com>
2019-08-26 04:34:34 +0800

    debug

--- lightningd/channel_control.c
+++ lightningd/channel_control.c
@@ -632,7 +632,7 @@ static void process_check_funding_broadcast(struct bitcoind *bitcoind UNUSED,
 	subd_send_msg(cancel->owner,
 		      take(towire_channel_send_error(NULL, error_reason)));
 	for (size_t i = 0; i < tal_count(cancel->forgets); i++)
-		assert(!forgets[i]->json_stream);
+		assert(!cancel->forgets[i]->json_stream);
 }
 
 struct command_result *cancel_channel_before_broadcast(struct command *cmd,
