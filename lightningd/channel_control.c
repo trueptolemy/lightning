7d1df46dba3e731982087ad80e52371c276cf1d0
trueptolemy <trueptolemy@foxmail.com>
2019-08-26 06:36:13 +0800

    Revert "debug"
    This reverts commit daef21ce282c37245351309bbaa337b38ad86aec.

--- lightningd/channel_control.c
+++ lightningd/channel_control.c
@@ -226,7 +226,7 @@ static void handle_error_channel(struct channel *channel,
 				 const int *fds)
 {
 	struct per_peer_state *pps;
-//	struct peer *peer = channel->peer;
+	struct peer *peer = channel->peer;
 //	struct command **forgets = tal_dup_arr(tmpctx, struct command *,
 //					       channel->forgets,
 //					       tal_count(channel->forgets), 0);
@@ -237,9 +237,9 @@ static void handle_error_channel(struct channel *channel,
 	struct command **forgets = tal_steal(tmpctx, channel->forgets);
 	for (size_t i = 0; i < tal_count(forgets); i++)
 		assert(!forgets[i]->json_stream);
-	tal_free(channel);
-	for (size_t i = 0; i < tal_count(forgets); i++)
-		assert(!forgets[i]->json_stream);
+//	tal_free(channel);
+//	for (size_t i = 0; i < tal_count(forgets); i++)
+//		assert(!forgets[i]->json_stream);
 
 	if (!fromwire_channel_send_error_reply(tmpctx, msg, &pps)) {
 		channel_internal_error(channel, "bad send_error_reply: %s",
@@ -252,10 +252,10 @@ static void handle_error_channel(struct channel *channel,
 		assert(!forgets[i]->json_stream);
 
 	/* Forget the channel. */
-//	delete_channel_direct(channel);
+	delete_channel_direct(channel);
 	/* Begin openingd again to keep peer connected. */
 
-/*	for (size_t i = 0; i < tal_count(forgets); i++)
+	for (size_t i = 0; i < tal_count(forgets); i++)
 		assert(!forgets[i]->json_stream);
 
 	peer_start_openingd(peer, pps, NULL);
@@ -269,7 +269,7 @@ static void handle_error_channel(struct channel *channel,
 		was_pending(command_success(forgets[i], response));
 		tal_free(response);
 	}
-*/
+
 	tal_free(forgets);
 }
 
