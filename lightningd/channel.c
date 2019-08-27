3d40d98bf939e9a169718ca2abf8ae0ccd8c4b7c
trueptolemy <trueptolemy@foxmail.com>
2019-08-28 02:40:53 +0800

    fix

--- lightningd/channel.c
+++ lightningd/channel.c
@@ -385,22 +385,18 @@ void channel_fail_permanent(struct channel *channel, const char *fmt, ...)
 		channel->error = towire_errorfmt(channel, &cid, "%s", why);
 	}
 
-//	if (channel->remote_funding_locked || channel->scid) {
-//		channel_set_owner(channel, NULL);
-//		/* Drop non-cooperatively (unilateral) to chain. */
-//		drop_to_chain(ld, channel, false);
-//
-//		if (channel_active(channel))
-//			channel_set_state(channel, channel->state, AWAITING_UNILATERAL);
-//	} else
-//		delete_channel(channel);
+	/* We should immediately forget the channel if we receive error during
+	 * CHANNELD_AWAITING_LOCKIN if we are fundee. */
+	if (channel->funder == REMOTE && channel->state == CHANNELD_AWAITING_LOCKIN)
+		delete_channel(channel);
+	else {
+		channel_set_owner(channel, NULL);
+		/* Drop non-cooperatively (unilateral) to chain. */
+		drop_to_chain(ld, channel, false);
 
-	channel_set_owner(channel, NULL);
-	/* Drop non-cooperatively (unilateral) to chain. */
-	drop_to_chain(ld, channel, false);
-
-	if (channel_active(channel))
-		channel_set_state(channel, channel->state, AWAITING_UNILATERAL);
+		if (channel_active(channel))
+			channel_set_state(channel, channel->state, AWAITING_UNILATERAL);
+	}
 
 	tal_free(why);
 }
