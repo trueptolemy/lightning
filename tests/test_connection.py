65e48a4b07726debe9e41c0004ae4f4bf13f1f3b
trueptolemy <trueptolemy@foxmail.com>
2019-08-27 18:31:01 +0800

    debug

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -873,8 +873,9 @@ def test_funding_external_wallet_corners(node_factory, bitcoind):
     assert l1.rpc.fundchannel_complete(l2.info['id'], prep['txid'], txout)['commitments_secured']
     # Canceld channel after fundchannel_complete
     assert l1.rpc.fundchannel_cancel(l2.info['id'])['cancelled']
-'''
+
     l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
+'''
     l1.rpc.fundchannel_start(l2.info['id'], amount)['funding_address']
     assert l1.rpc.fundchannel_complete(l2.info['id'], prep['txid'], txout)['commitments_secured']
     l1.rpc.txsend(prep['txid'])
