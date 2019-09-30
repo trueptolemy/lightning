3033f9b4dc8d2934d6de595b6b0b149d90519480
trueptolemy <trueptolemy@foxmail.com>
2019-10-01 03:02:48 +0800

    pytest: Test compact of the deprecated `fundchannel` and `fundchannel_start`

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -699,6 +699,51 @@ def test_shutdown_awaiting_lockin(node_factory, bitcoind):
     wait_for(lambda: l2.rpc.listpeers()['peers'] == [])
 
 
+@unittest.skipIf(not COMPAT, "needs COMPAT=1")
+def test_fundchannel_start_compact(node_factory, bitcoind):
+    """Test the deprecated old-style:
+       fundchannel {id} {satoshi} {feerate} {announce}
+    """
+    l1, l2, l3, l4, l5 = node_factory.get_nodes(5)
+
+    l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
+    l1.rpc.connect(l3.info["id"], "localhost", l3.port)
+    l1.rpc.connect(l4.info["id"], "localhost", l4.port)
+    l1.rpc.connect(l5.info["id"], "localhost", l5.port)
+
+    l1.rpc.fundchannel_start(id=l2.info["id"], satoshi=10**6, feerate=7500)
+    l1.rpc.fundchannel_start(l3.info["id"], satoshi=10**6)
+    l1.rpc.fundchannel_start(l4.info["id"], 10**6, feerate=7500)
+    l1.rpc.fundchannel_start(l5.info["id"], 10**6)
+
+
+@unittest.skipIf(not COMPAT, "needs COMPAT=1")
+def test_fundchannel_compact(node_factory, bitcoind):
+    """Test the deprecated old-style:
+       fundchannel {id} {satoshi} {feerate} {announce} {minconf} {utxos}
+    """
+    l1, l2, l3, l4, l5, l6 = node_factory.get_nodes(6)
+
+    # Get 2 utxos
+    l1.fundwallet(0.01 * 10**8)
+    l1.fundwallet(0.01 * 10**8)
+    wait_for(lambda: len(l1.rpc.listfunds()["outputs"]) == 2)
+
+    utxos = [utxo["txid"] + ":" + str(utxo["output"]) for utxo in l1.rpc.listfunds()["outputs"]]
+
+    l1.rpc.connect(l2.info['id'], 'localhost', l2.port)
+    l1.rpc.connect(l3.info["id"], "localhost", l3.port)
+    l1.rpc.connect(l4.info["id"], "localhost", l4.port)
+    l1.rpc.connect(l5.info["id"], "localhost", l5.port)
+    l1.rpc.connect(l6.info["id"], "localhost", l6.port)
+
+    l1.rpc.fundchannel(l2.info["id"], satoshi="all", feerate=7500, utxos=[utxos[0]])['channel_id']
+    l1.rpc.fundchannel(l3.info["id"], satoshi=int(0.007 * 10**8), feerate=1000, utxos=[utxos[1]])['channel_id']
+    l1.rpc.fundchannel(id=l4.info["id"], satoshi=int(0.007 * 10**8))['channel_id']
+    l1.rpc.fundchannel(l5.info["id"], 10**6, announce=True)['channel_id']
+    l1.rpc.fundchannel(l6.info["id"], 10**6)['channel_id']
+
+
 def test_funding_change(node_factory, bitcoind):
     """Add some funds, fund a channel, and make sure we remember the change
     """
